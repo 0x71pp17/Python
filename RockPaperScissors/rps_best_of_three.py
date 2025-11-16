5d62de6dc1605ecd7d44b651b1cfccbe0f912031
0x71pp17 <tippitj@gmail.com>
2025-11-15 20:37:21 -0500

    Create rps_best_of_three.py

---
+++ RockPaperScissors/rps_best_of_three.py
@@ -0,0 +1,38 @@
+import random
+
+player_wins = 0
+computer_wins = 0
+
+print("Let's play rock, paper, or scissors")
+
+choices = ["rock", "paper", "scissors"]
+
+while player_wins < 2 and computer_wins < 2:
+    player_choice = input("Choose rock, paper, or scissors: ").lower()
+
+    computer_choice = random.choice(choices)
+
+    print (f"Computer chose: {computer_choice}")
+
+    if (player_choice == "rock" and computer_choice == "scissors") or (player_choice == "scissors" and computer_choice == "paper") or (player_choice == "paper" and computer_choice == "rock"):
+        winner = "Player"
+    elif player_choice == computer_choice:
+        winner = "Tie"
+    else:
+        winner = "Computer"   
+
+    if winner == "Player":
+        print("You won")
+        player_wins += 1
+    elif winner == "Computer":
+        print("Computer won")
+        computer_wins += 1
+    else:
+        print("It's a tie")   
+
+    print(f"Current Score - Player: {player_wins}, Computer: {computer_wins}")
+
+if player_wins > computer_wins:
+    print("Congratulations!")
+else:
+    print("Computer won!")
